d39d714635f378a440159d41645be15605b60c2c
J8051 <nathaniel.jessica@yahoo.ca>
2023-11-02 09:37:44 -0400

    db setup & react setup

---
+++ db/seeds/01_notes.sql
@@ -0,0 +1,11 @@
+INSERT INTO notes (name)
+VALUES ('Call Mom');
+
+INSERT INTO notes (name)
+VALUES ('Study for Test');
+
+INSERT INTO notes (name)
+VALUES ('Visit Uncle Bob');
+
+INSERT INTO notes (name)
+VALUES ('Research Vacation Choices');
